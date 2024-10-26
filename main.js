82386d99cae4a790d8deda9b2691831388f0183e
Світлана Ем <svitlanaem@MacBook-Air-Svitlana.local>
2024-10-26 14:13:54 +0900

    Remove comments

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 import $app from "./src/constants/app.js"
 import Newsletter from "./src/modules/Newsletter/Newsletter.js"
-import ThankMessage from "./src/modules/ThankMessage/ThankMessage.js"
 import "./main.scss"
 
 const newLetter = new Newsletter()
 
 $app.appendChild(newLetter.newsletterWrapper)
-
-// const thank = new ThankMessage('jhkjh')
-
-// $app.appendChild(thank.thankMessage)
